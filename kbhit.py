49befbca0802c674fcb3ae92c1e2a15217baa9cd
Jang Suk Woo <iwkdtjrdn@gmail.com>
2020-10-27 19:53:53 +0900

    2018

---
+++ kbhit.py
@@ -0,0 +1,120 @@
+import os
+
+# Windows
+if os.name == 'nt':
+    import msvcrt
+
+# Posix (Linux, OS X)
+else:
+    import sys
+    import termios
+    import atexit
+    from select import select
+
+
+class KBHit:
+
+    def __init__(self):
+        '''Creates a KBHit object that you can call to do various keyboard things.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+
+            # Save the terminal settings
+            self.fd = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.fd)
+            self.old_term = termios.tcgetattr(self.fd)
+
+            # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+
+            # Support normal-terminal reset at exit
+            atexit.register(self.set_normal_term)
+
+    def set_normal_term(self):
+        ''' Resets to normal terminal.  On Windows this is a no-op.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+    def getch(self):
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow().
+        '''
+
+        s = ''
+
+        if os.name == 'nt':
+            return msvcrt.getch().decode('utf-8')
+        else:
+            return sys.stdin.read(1)
+
+    def getarrow(self):
+        ''' Returns an arrow-key code after kbhit() has been called. Codes are
+        0 : up
+        1 : right
+        2 : down
+        3 : left
+        Should not be called in the same program as getch().
+        '''
+
+        if os.name == 'nt':
+            d = msvcrt.getch()  # skip 0xE0
+            if d == b'\xe0':
+                # print("Got it!", d)
+                c = msvcrt.getch()
+                # vals = [72, 77, 80, 75]
+                vals = [75, 80, 77, 72]
+                return vals.index(ord(c))
+            else:
+                # print('No!!!', d)
+                return ord(d);
+        else:
+            c = sys.stdin.read(3)[2]
+            # vals = [65, 67, 66, 68]
+            vals = [68, 66, 67, 65]
+
+        # print("Key in==>", c, "ord(c)=", ord(c), "vals.index(ord(c))=", vals.index(ord(c)))
+
+        return vals.index(ord(c.decode('utf-8')))
+
+    def kbhit(self):
+        ''' Returns True if keyboard character was hit, False otherwise.
+        '''
+        if os.name == 'nt':
+            return msvcrt.kbhit()
+
+        else:
+            dr, dw, de = select([sys.stdin], [], [], 0)
+            return dr != []
+
+
+# Test
+if __name__ == "__main__":
+
+    kb = KBHit()
+
+    print('Hit any key, or ESC to exit')
+
+    while True:
+
+        if kb.kbhit():
+            # c = kb.getch()
+            # if ord(c) == 27: # ESC
+            #    break
+
+            c = kb.getarrow()
+            print("Final c value==>", c)
+            if c == 27:  # ESC
+                break
+            print(c)
+
+    kb.set_normal_term()
